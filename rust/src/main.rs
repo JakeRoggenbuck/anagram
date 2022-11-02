648232fc6004ebec04fba6e50829ffba40c84b15
Jake <jakeroggenbuck2@gmail.com>
2022-11-02 13:54:30 -0700

    Add largest finder

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
@@ -14,6 +15,27 @@ fn main() -> io::Result<()> {
         let h = ol.chars().sorted().collect::<String>();
         let k = words.entry(h).or_insert(Vec::<String>::new());
         k.push(ol);
+    }
+
+    let mut keyvec: Vec<String> = words
+        .iter()
+        .filter(|v| v.1.len() > 1)
+        .map(|x| x.0)
+        .cloned()
+        .collect();
+
+    keyvec.sort_by(|a, b| {
+        if a.len() > b.len() {
+            Ordering::Less
+        } else if a.len() == b.len() {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
+    });
+
+    for x in keyvec.iter().take(200) {
+        println!("{:?}", words[x]);
     }
 
     Ok(())
